9f13047bf75c79f7c7a48625213af64e86ad1efa
Bodo Bogdan-Mihai <80922283+waterAddicted@users.noreply.github.com>
2023-05-03 09:59:58 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataParent = new System.Windows.Forms.DataGridView();
+            this.dataChild = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.pnlChild = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataParent
+            // 
+            this.dataParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataParent.Location = new System.Drawing.Point(9, 10);
+            this.dataParent.Margin = new System.Windows.Forms.Padding(2);
+            this.dataParent.Name = "dataParent";
+            this.dataParent.RowHeadersWidth = 51;
+            this.dataParent.RowTemplate.Height = 24;
+            this.dataParent.Size = new System.Drawing.Size(369, 164);
+            this.dataParent.TabIndex = 0;
+            this.dataParent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataParent_CellContentClick);
+            this.dataParent.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataParent_SelectionChanged);
+            // 
+            // dataChild
+            // 
+            this.dataChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataChild.Location = new System.Drawing.Point(9, 179);
+            this.dataChild.Margin = new System.Windows.Forms.Padding(2);
+            this.dataChild.Name = "dataChild";
+            this.dataChild.RowHeadersWidth = 51;
+            this.dataChild.RowTemplate.Height = 24;
+            this.dataChild.Size = new System.Drawing.Size(369, 177);
+            this.dataChild.TabIndex = 1;
+            this.dataChild.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataChild_CellContentClick);
+            this.dataChild.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataChild_SelectionChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(400, 206);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(56, 27);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(460, 206);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(56, 27);
+            this.btnRemove.TabIndex = 12;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(521, 206);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(56, 27);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(464, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 18);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Parent";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(464, 179);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 18);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Child";
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(460, 38);
+            this.btnDisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(56, 24);
+            this.btnDisplay.TabIndex = 22;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // pnlChild
+            // 
+            this.pnlChild.Location = new System.Drawing.Point(392, 238);
+            this.pnlChild.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlChild.Name = "pnlChild";
+            this.pnlChild.Size = new System.Drawing.Size(264, 153);
+            this.pnlChild.TabIndex = 33;
+            this.pnlChild.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlChild_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 439);
+            this.Controls.Add(this.pnlChild);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataChild);
+            this.Controls.Add(this.dataParent);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataParent;
+        private System.Windows.Forms.DataGridView dataChild;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Panel pnlChild;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab6
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+
+        SqlDataAdapter daParent = new SqlDataAdapter();
+        SqlDataAdapter daChild = new SqlDataAdapter();
+        DataSet dsParent = new DataSet();
+        DataSet dsChild = new DataSet();
+        BindingSource bsParent = new BindingSource();
+        BindingSource bsChild = new BindingSource();
+
+        string ParentID = ConfigurationManager.AppSettings["ParentID"];
+        string ChildID = ConfigurationManager.AppSettings["ChildID"];
+        List<string> ChildColumnsList = new List<string>(ConfigurationManager.AppSettings["ChildColumns"].Split(','));
+        string SelectParent = ConfigurationManager.AppSettings["SelectParent"];
+        string SelectChild = ConfigurationManager.AppSettings["SelectChild"];
+        string AddChild = ConfigurationManager.AppSettings["AddChild"];
+        string RemoveChild = ConfigurationManager.AppSettings["RemoveChild"];
+        string UpdateChild = ConfigurationManager.AppSettings["UpdateChild"];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int pointY = 0;
+            pnlChild.Controls.Clear();
+
+            foreach (string column in ChildColumnsList)
+            {
+                Label lbl = new Label();
+                lbl.Text = column;
+                lbl.AutoSize = true;
+                lbl.Location = new Point(0, pointY);
+
+                TextBox txt = new TextBox();
+                txt.Name = column;
+                txt.Location = new Point(90, pointY);
+
+                pnlChild.Controls.Add(lbl);
+                pnlChild.Controls.Add(txt);
+                pnlChild.Show();
+
+                pointY += 20;
+            }
+
+            pointY += 10;
+
+            Label l = new Label();
+            l.Text = "Change " + ParentID;
+            l.AutoSize = true;
+            l.Location = new Point(0, pointY);
+
+            TextBox t = new TextBox();
+            t.Name = ParentID;
+            t.Width = 50;
+            t.Location = new Point(120, pointY);
+
+            pnlChild.Controls.Add(l);
+            pnlChild.Controls.Add(t);
+            pnlChild.Show();
+        }
+
+        private void clearTextboxes()
+        {
+            foreach (string column in ChildColumnsList)
+            {
+                TextBox textBox = (TextBox)pnlChild.Controls[column];
+                textBox.Text = "";
+            }
+            TextBox txt = (TextBox)pnlChild.Controls[ParentID];
+            txt.Text = "";
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            daParent.SelectCommand = new SqlCommand(SelectParent, cs);
+
+            dsParent.Clear();
+            daParent.Fill(dsParent);
+            dataParent.DataSource = dsParent.Tables[0];
+            bsParent.DataSource = dsParent.Tables[0];
+        }
+
+        private void dataParent_SelectionChanged(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand(SelectChild, cs);
+
+            cmd.Parameters.AddWithValue("@" + ParentID, dataParent.CurrentRow.Cells[0].Value);
+
+            daChild.SelectCommand = cmd;
+
+            dsChild.Clear();
+            daChild.Fill(dsChild);
+            dataChild.DataSource = dsChild.Tables[0];
+            bsChild.DataSource = dsChild.Tables[0];
+
+            clearTextboxes();
+        }
+
+        private void dataChild_SelectionChanged(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int i = 1;
+            foreach (string column in ChildColumnsList)
+                pnlChild.Controls[column].Text = dataChild.CurrentRow.Cells[i++].Value.ToString();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(AddChild, cs);
+
+                cmd.Parameters.AddWithValue("@" + ParentID, dataParent.CurrentRow.Cells[0].Value);
+
+                foreach (string column in ChildColumnsList)
+                {
+                    TextBox textBox = (TextBox)pnlChild.Controls[column];
+                    cmd.Parameters.AddWithValue("@" + column, textBox.Text);
+                }
+
+                cs.Open();
+                cmd.ExecuteNonQuery();
+                cs.Close();
+
+                MessageBox.Show("Record was added!");
+                clearTextboxes();
+
+                dsChild.Clear();
+                daChild.Fill(dsChild);
+                dataChild.DataSource = dsChild.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cs.Close();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            DialogResult dr;
+            dr = MessageBox.Show("Are you sure?", "Confirm removal", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                SqlCommand cmd = new SqlCommand(RemoveChild, cs);
+
+                cmd.Parameters.AddWithValue("@" + ChildID, dataChild.CurrentRow.Cells[0].Value);
+
+                cs.Open();
+                cmd.ExecuteNonQuery();
+                cs.Close();
+
+                MessageBox.Show("Record was removed!");
+                clearTextboxes();
+
+                dsChild.Clear();
+                daChild.Fill(dsChild);
+                dataChild.DataSource = dsChild.Tables[0];
+            }
+            else
+                MessageBox.Show("Record was not removed!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(UpdateChild, cs);
+
+                cmd.Parameters.AddWithValue("@" + ChildID, dataChild.CurrentRow.Cells[0].Value);
+
+                TextBox txt = (TextBox)pnlChild.Controls[ParentID];
+                if (txt.Text == "")
+                    cmd.Parameters.AddWithValue("@" + ParentID, dataParent.CurrentRow.Cells[0].Value);
+                else
+                    cmd.Parameters.AddWithValue("@" + ParentID, txt.Text);
+
+                foreach (string column in ChildColumnsList)
+                {
+                    TextBox textBox = (TextBox)pnlChild.Controls[column];
+                    cmd.Parameters.AddWithValue("@" + column, textBox.Text);
+                }
+
+                cs.Open();
+                cmd.ExecuteNonQuery();
+                cs.Close();
+
+                MessageBox.Show("Record was updated!");
+                clearTextboxes();
+
+                dsChild.Clear();
+                daChild.Fill(dsChild);
+                dataChild.DataSource = dsChild.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cs.Close();
+            }
+        }
+
+        private void dataParent_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataChild_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pnlChild_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
